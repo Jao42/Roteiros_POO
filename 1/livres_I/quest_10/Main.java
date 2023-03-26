81f7c9dc264303f5bae992d067d59dd6567cba59
Jao42 <joaovictormedeiros41@gmail.com>
2023-03-26 17:20:35 -0300

    questao 10

---
+++ 1/livres_I/quest_10/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int num = entrada.nextInt();
+    if ((num % 10) == 0)
+      System.out.println("Eh divisivel por 2 e 5");
+
+
+  }
+}
