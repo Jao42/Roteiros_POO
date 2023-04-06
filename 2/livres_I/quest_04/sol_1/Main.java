c644ba6043e524af824d197c3260fe1cc41f78d9
Jao42 <joaovictormedeiros41@gmail.com>
2023-04-06 17:54:51 -0300

    R2L1Q4 solucao 1

---
+++ 2/livres_I/quest_04/sol_1/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    double soma = entrada.nextDouble() + entrada.nextDouble();
+    System.out.printf("%.2f", soma);
+    
+
+  }
+}
